8c6756476a9363ba323c517b4b6daf9d68147de7
Onur Kaçmaz <onurkacmaz@HotelRunners-MacBook-Pro.local>
2024-03-26 11:35:22 +0300

    change db adapter

--- Gemfile
+++ Gemfile
@@ -9,7 +9,7 @@ gem "rails", "~> 7.1.3", ">= 7.1.3.2"
 gem "sprockets-rails"
 
 # Use sqlite3 as the database for Active Record
-gem "sqlite3", "~> 1.4"
+gem "pg"
 
 # Use the Puma web server [https://github.com/puma/puma]
 gem "puma", ">= 5.0"
@@ -67,6 +67,5 @@ group :test do
   gem "selenium-webdriver"
 end
 
-gem 'devise'
 gem 'omniauth-google-oauth2'
 gem 'dotenv-rails', groups: [:development, :test]
--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,25 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 20_240_325_185_224) do
-  create_table 'tasks', force: :cascade do |t|
-    t.string 'title'
-    t.text 'description'
-    t.boolean 'isCompleted'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.integer 'user_id', null: false
-    t.index ['user_id'], name: 'index_tasks_on_user_id'
+ActiveRecord::Schema[7.1].define(version: 2024_03_25_185224) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.boolean "isCompleted"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
-  create_table 'auth', force: :cascade do |t|
-    t.string 'name'
-    t.boolean 'status'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.string 'email'
-    t.string 'password'
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.boolean "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "email"
+    t.string "password"
   end
 
-  add_foreign_key 'tasks', 'auth'
+  add_foreign_key "tasks", "users"
 end
